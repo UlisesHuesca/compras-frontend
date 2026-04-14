0dc90373dedd3a3bd33720ccd31755e77828058b
ulises.huesca <ulises.huesc@hotmail.com>
2026-04-14 11:59:28 -0600

    Se quita el route innecesario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { Container, CssBaseline, Button, Stack, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import { Routes, Route, Navigate } from "react-router-dom";
-
 import ComprasResumenPage from "./pages/ComprasResumenPage";
 
 
@@ -11,11 +9,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container maxWidth="xl" sx={{ py: 4 }}>
-        <Routes>
-          <Route path="/" element={<Navigate to="/compras/resumen" replace />} />
-          <Route path="/compras/resumen" element={<ComprasResumenPage />} />
-         
-        </Routes>
+          <ComprasResumenPage />
       </Container>
     </ThemeProvider>
   );
